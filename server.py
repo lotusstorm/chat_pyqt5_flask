062598f5f1d341778fc79ded381f1bd77e1d2ce7
lotusstorm <iutizev@gmail.com>
2018-07-20 17:11:36 +0300

    fix_2

--- server.py
+++ server.py
@@ -1,13 +1,10 @@
-import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_marshmallow import Marshmallow
 
-file_path = os.path.abspath(os.getcwd())+"\chat6.db"
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat6.db'
 
 
 db = SQLAlchemy(app)
